3c02d336cc32e32dd114c6b4201e406970ac6393
Mattias Bälter <mattias.balter@eficode.com>
2026-02-17 08:50:57 +0100

    Added facivon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Frida & Mattias 2026",
   description: "KALAS",
+  icons: {
+    icon: "/icon.png"
+  }
 };
 
 export default function RootLayout({
